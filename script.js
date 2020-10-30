1194fa880615359d63ea9444d41b1e30d88c5e48
facundo elias lopez <facueliaslopez@hotmail.com>
2020-10-30 08:29:03 -0300

    Botones, funcionabilidad y demás a CURSOS Y EDUCACION

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let btn = document.querySelector("#boton")
 btn.addEventListener('click', function () {
   
     if (about.classList.contains('oculto')) {
+      
       about.classList.remove('oculto')
+      
     }
     else
     {
@@ -22,6 +24,34 @@ btn2.addEventListener('click', function () {
     else
     {
         media.classList.add('oculto');
+    }
+    
+  });
+
+let edu = document.querySelector(".educacion")
+let btn3 = document.querySelector("#boton3")
+btn3.addEventListener('click', function () {
+  
+    if (edu.classList.contains('oculto')) {
+      edu.classList.remove('oculto')
+    }
+    else
+    {
+        edu.classList.add('oculto');
+    }
+    
+  });
+
+let cursos = document.querySelector(".cursos")
+let btn4 = document.querySelector("#boton4")
+btn4.addEventListener('click', function () {
+  
+    if (cursos.classList.contains('oculto')) {
+      cursos.classList.remove('oculto')
+    }
+    else
+    {
+        cursos.classList.add('oculto');
     }
     
   });
